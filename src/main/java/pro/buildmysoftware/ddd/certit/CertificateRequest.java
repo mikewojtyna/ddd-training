e262ade7ed1ad0254e0801527f77ee2b1d59bfe4
Mike Wojtyna <mike@buildmysoftware.pro>
2019-04-08 00:07:41 +0200

    Write basic request certificate and pay use cases

--- src/main/java/pro/buildmysoftware/ddd/certit/CertificateRequest.java
+++ src/main/java/pro/buildmysoftware/ddd/certit/CertificateRequest.java
@@ -1,4 +1,25 @@
 package pro.buildmysoftware.ddd.certit;
 
+import java.time.LocalDate;
+
 public class CertificateRequest {
+	private LocalDate examDate;
+	private boolean examPaid;
+
+	public void scheduleExam(LocalDate currentDate) {
+		examDate = currentDate.plusMonths(1);
+		examPaid = false;
+	}
+
+	public LocalDate examDate() {
+		return examDate;
+	}
+
+	public boolean canCustomerTakeExam() {
+		return examPaid;
+	}
+
+	public void pay() {
+		examPaid = true;
+	}
 }
--- src/test/java/pro/buildmysoftware/ddd/certit/RequestCertificateTest.java
+++ src/test/java/pro/buildmysoftware/ddd/certit/RequestCertificateTest.java
@@ -3,13 +3,15 @@ package pro.buildmysoftware.ddd.certit;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RequestCertificateTest {
 
 	@DisplayName("customer can request a new certificate")
 	@Test
-	void test() throws Exception {
+	void test0() throws Exception {
 		// given
 		CertificatesOffice office = certificateRequestService();
 		Customer customer = anyCustomer();
@@ -20,6 +22,84 @@ public class RequestCertificateTest {
 
 		// then
 		assertThat(certificateRequest).isNotNull();
+	}
+
+	@DisplayName("given certificate request, the exam can be scheduled")
+	@Test
+	void test1() throws Exception {
+		// given
+		CertificateRequest certificateRequest = requestCertificate();
+		LocalDate currentDate = LocalDate.of(2019, 1, 16);
+
+		// when
+		certificateRequest.scheduleExam(currentDate);
+
+		// then
+		assertThat(certificateRequest.examDate())
+			.isEqualTo(LocalDate.of(2019, 2, 16));
+	}
+
+	// @formatter:off
+	@DisplayName(
+		"given certificate request with scheduled exam, " +
+		"and exam is not paid, " +
+		"then user cannot take the exam"
+	)
+	// @formatter:on
+	@Test
+	void test2() throws Exception {
+		// given
+		CertificateRequest certificateRequest =
+			notPaidCertificateRequest();
+
+		// when
+		boolean userCanTakeExam = certificateRequest
+			.canCustomerTakeExam();
+
+		// then
+		assertThat(userCanTakeExam).isFalse();
+	}
+
+	// @formatter:off
+	@DisplayName(
+		"given certificate request with scheduled exam, " +
+		"when exam is paid, " +
+		"then user can take the exam"
+	)
+	// @formatter:on
+	@Test
+	void test3() throws Exception {
+		// given
+		CertificateRequest certificateRequest =
+			paidCertificateRequest();
+
+		// when
+		boolean canCustomerTakeExam = certificateRequest
+			.canCustomerTakeExam();
+
+		// then
+		assertThat(canCustomerTakeExam).isTrue();
+	}
+
+	private CertificateRequest paidCertificateRequest() {
+		CertificateRequest certificateRequest = requestCertificate();
+		certificateRequest.pay();
+		return certificateRequest;
+	}
+
+	private CertificateRequest notPaidCertificateRequest() {
+		CertificateRequest certificateRequest = requestCertificate();
+		certificateRequest.scheduleExam(anyDate());
+		return certificateRequest;
+	}
+
+	private LocalDate anyDate() {
+		return LocalDate.of(2010, 10, 10);
+	}
+
+	private CertificateRequest requestCertificate() {
+		return certificateRequestService()
+			.requestCertificateBy(anyCustomer());
 	}
 
 	private Customer anyCustomer() {
