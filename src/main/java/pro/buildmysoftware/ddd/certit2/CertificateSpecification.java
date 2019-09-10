be4f51d30a36198c5da270d1d661ca0063c1f0ae
Mike Wojtyna <mike@buildmysoftware.pro>
2019-09-10 15:27:12 +0200

    Add certificate specification example

---
+++ src/main/java/pro/buildmysoftware/ddd/certit2/CertificateSpecification.java
@@ -0,0 +1,15 @@
+package pro.buildmysoftware.ddd.certit2;
+
+public interface CertificateSpecification {
+	boolean isSatisfiedBy(Certificate certificate);
+
+	default CertificateSpecification and(CertificateSpecification certificateSpecification) {
+		return certificate -> isSatisfiedBy(certificate) && certificateSpecification
+			.isSatisfiedBy(certificate);
+	}
+
+	default CertificateSpecification or(CertificateSpecification certificateSpecification) {
+		return certificate -> isSatisfiedBy(certificate) || certificateSpecification
+			.isSatisfiedBy(certificate);
+	}
+}
